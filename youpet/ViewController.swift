e0a772af68eecf7a39d44d7313ce0c9b6539d04e
_tiagoaguiar_ <tiago.aguiar@moonjava.com.br>
2017-10-03 23:00:25 -0300

    programatically ios

--- youpet/ViewController.swift
+++ youpet/ViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-
+    let tbl: UITableView = {
+        let v = UITableView(frame: .zero, style: .plain)
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
     let dogs = ["Pastor Alemão","Pastor Belga",
                 "Pastor de Brie","Pastor dos Pirenéus de Cara Rosa","Pequinês",
                 "Perdigueiro","Pitbull","Podengo","Pointer","Pug"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tbl)
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|",
+                                                           options: NSLayoutFormatOptions(),
+                                                           metrics: nil,
+                                                           views: ["v0" : tbl]))
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|",
+                                                           options: NSLayoutFormatOptions(),
+                                                           metrics: nil,
+                                                           views: ["v0" : tbl]))
+        
+        tbl.delegate = self
+        tbl.dataSource = self
+        tbl.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
